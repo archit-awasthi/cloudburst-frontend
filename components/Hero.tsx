b53113340fda14e3cfc0a495e258e977864fcf84
archit <architawasthims7@gmail.com>
2025-12-20 13:48:32 +0530

    new merge

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -89,7 +89,7 @@ export const Hero: React.FC<HeroProps> = ({ onShowReport }) => {
         {/* Title */}
         <div className="flex flex-col items-center">
           <h1 className="text-6xl md:text-8xl font-bold tracking-tight text-brand-cream drop-shadow-2xl">
-            Adamancy takes you no where
+            New Title
           </h1>
         </div>
 
